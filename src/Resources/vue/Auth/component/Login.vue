844fa77cf16afa60441d9b33da5bfdb40fbffc4c
RaminRoshan <ramin.roshanghiyas@gmail.com>
2023-11-20 21:04:44 +0330

    قالب first اضافه شد

--- src/Resources/vue/Auth/component/Login.vue
+++ src/Resources/vue/Auth/component/Login.vue
@@ -1,39 +1,36 @@
 <template>
-  <div class="row justify-content-md-center">
-    <div class="col-xl-4 col-lg-5 col-md-6 col-sm-12">
-      <form method="post" name="login" :action="redirect" ref="loginForm">
-      </form>
-      <div class="login-screen">
-        <div class="login-box">
-          <a href="#" class="login-logo">
-            <img :src="getAppUrl() + '/templates/Wafi_Admin/img/logo-dark.png'" alt="Wafi Admin Dashboard">
-          </a>
-          <h5 v-html="translateText('welcomeMessage')"></h5>
-          <div class="form-group">
-            <input type="text" class="form-control" :placeholder="translateText('usernamePlaceholder')" v-model="username" autocomplete="username" minlength="3" required>
-          </div>
-          <div class="form-group">
-            <input type="password" class="form-control" :placeholder="translateText('passwordPlaceholder')" v-model="password" autocomplete="current-password" minlength="6" required>
-          </div>
-          <div class="actions mb-4">
-            <div class="custom-control custom-checkbox">
-              <input type="checkbox" class="custom-control-input" id="remember_pwd">
-              <label class="custom-control-label" for="remember_pwd">{{ translateText('rememberMe') }}</label>
-            </div>
-            <button type="button" class="btn btn-primary" @click="userLogin">{{ translateText('loginButton') }}</button>
-          </div>
-          <div class="forgot-pwd">
-            <button class="btn-link" @click="showAlert('attention', 'accessDenied', 'error', 'close')">{{ translateText('forgotPassword') }}</button>
-          </div>
-          <hr>
-          <div class="actions align-left">
-            <span class="additional-link">{{ translateText('signUpMessage') }}</span>
-            <button class="btn btn-dark" @click="showAlert('attention', 'accessDenied', 'error', 'close')">{{ translateText('signUpButton') }}</button>
-          </div>
-        </div>
-      </div>
-    </div>
+  <div class="mb-3 fv-plugins-icon-container">
+    <label for="email" class="form-label">{{translateText('username')}}:</label>
+    <input type="text" class="form-control text-start" id="email" v-model="username" :placeholder="translateText('usernamePlaceholder')" autofocus="" dir="ltr">
+    <div class="fv-plugins-message-container invalid-feedback"></div>
   </div>
+  <div class="mb-3 form-password-toggle fv-plugins-icon-container">
+    <div class="d-flex justify-content-between">
+      <label class="form-label" for="password">{{translateText('password')}}:</label>
+      <a style="color:blue;" @click="showAlert('attention', 'accessDenied', 'error', 'close')">
+        <small>{{ translateText('forgotPassword') }}</small>
+      </a>
+    </div>
+    <div class="input-group input-group-merge has-validation">
+      <input type="password" id="password" class="form-control text-start" v-model="password" :placeholder="translateText('passwordPlaceholder')" aria-describedby="password" dir="ltr">
+      <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
+    </div>
+    <div class="fv-plugins-message-container invalid-feedback"></div>
+  </div> 
+  <div class="mb-3">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="remember-me">
+      <label class="form-check-label" for="remember-me"> {{ translateText('rememberMe') }} </label>
+    </div>
+  </div> 
+  <button class="btn btn-primary d-grid w-100" @click="userLogin">{{ translateText('loginButton') }}</button> 
+  <br>
+  <p class="text-center">
+    <span>{{ translateText('signUpMessage') }}</span>
+    <a style="color:blue;" @click="showAlert('attention', 'accessDenied', 'error', 'close')">
+      <span>{{ translateText('signUpButton') }}</span>
+    </a>
+  </p> 
 </template>
 
 <script>
